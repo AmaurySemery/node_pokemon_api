0e9c4f7c195c9168459a5a4c6b7401430c3f5169
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-13 21:40:42 +0100

    Ajout de premières routes

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express !'))
 
+app.get('/api/pokemon/:id/:name', (req, res) => {
+    const id = req.params.id
+    const name = req.params.name
+    res.send(`Vous avez demandé le pokémon n°${id} est ${name}.`)
+})
+
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
