a17272d9a6f1b42a1c1fc460ec9d128f9ba92dec
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-13 21:58:56 +0100

    Découvrir les routes avec Express

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
+let pokemons = require ('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express !'))
 
-app.get('/api/pokemon/:id/:name', (req, res) => {
-    const id = req.params.id
-    const name = req.params.name
-    res.send(`Vous avez demandé le pokémon n°${id} est ${name}.`)
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}.`)
+})
+
+app.get('/api/pokemons', (req, res) => {
+    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex pour le moment.`)
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
