b67a1a65aee8f93e127766218b8056b1aa7c7146
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-15 19:35:08 +0100

    Réagencement du code

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const { success } = require('./helper')
 let pokemons = require ('./mock-pokemon')
 
 const app = express()
 const port = 3000
+
+app.use(morgan('dev'))
 
 app.use((req, res, next) => {
   console.log(`URL : ${req.url}`)
@@ -12,16 +15,16 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => res.send('Hello, Express !'))
 
+app.get('/api/pokemons', (req, res) => {
+  const message = 'La liste des pokémons a bien été récupérée.'
+  res.json(success(message, pokemons)) 
+})
+
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     const message = 'Un pokémon a bien été trouvé.'
     res.json(success(message, pokemon))
 })
-
-app.get('/api/pokemons', (req, res) => {
-    const message = 'La liste des pokémons a bien été récupérée.'
-    res.json(success(message, pokemons)) 
-  })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
