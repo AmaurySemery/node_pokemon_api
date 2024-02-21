b02bda316c07aa26dff4b75deb552e294ac968ca
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-21 18:55:22 +0100

    Mise en forme

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const { success } = require('./helper')
-let pokemons = require ('./mock-pokemon')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app
-.use(favicon(__dirname + '/favicon.ico'))
-.use(morgan('dev'))
+  .use(favicon(__dirname + '/favicon.ico'))
+  .use(morgan('dev'))
 
 app.use((req, res, next) => {
   console.log(`URL : ${req.url}`)
@@ -20,14 +20,14 @@ app.get('/', (req, res) => res.send('Hello, Express !'))
 
 app.get('/api/pokemons', (req, res) => {
   const message = 'La liste des pokémons a bien été récupérée.'
-  res.json(success(message, pokemons)) 
+  res.json(success(message, pokemons))
 })
 
 app.get('/api/pokemons/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    const message = 'Un pokémon a bien été trouvé.'
-    res.json(success(message, pokemon))
+  const id = parseInt(req.params.id)
+  const pokemon = pokemons.find(pokemon => pokemon.id === id)
+  const message = 'Un pokémon a bien été trouvé.'
+  res.json(success(message, pokemon))
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
