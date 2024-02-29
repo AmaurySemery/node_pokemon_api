69c1e2f3b0442da9e1819a808121d307b9e60e9c
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-29 14:51:40 +0100

    Erreur 404 findPokemonByPk

--- src/routes/findPokemonByPk.js
+++ src/routes/findPokemonByPk.js
@@ -4,6 +4,10 @@ module.exports = (app) => {
   app.get('/api/pokemons/:id', (req, res) => {
     Pokemon.findByPk(req.params.id)
       .then(pokemon => {
+        if(pokemon === null) {
+          const message = 'Le pokémon demandé n\'existe pas. Réessayez avec un autre identifiant.'
+          return res.statut(404).json({message})
+        }
         const message = 'Un pokémon a bien été trouvé.'
         res.json({ message, data: pokemon })
       })
